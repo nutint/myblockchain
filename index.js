8cca17ee1e048e6bfccabf4aed6ff9075f8bec2f
Natthapong Intharak <nattha.int@gmail.com>
2019-11-13 01:24:34 +0700

    sync chain

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const request = require('request')
 const Blockchain = require('./blockchain')
 const bodyParser = require('body-parser')
 const PubSub = require('./pubsub')
@@ -6,6 +7,9 @@ const PubSub = require('./pubsub')
 const app = express()
 const blockchain = new Blockchain()
 const pubsub = new PubSub({blockchain})
+
+const DEFAULT_PORT=3000
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
 
 setTimeout(() => pubsub.broadcastChain(), 1000)
 
@@ -22,9 +26,19 @@ app.post('/api/mine', (req, res) => {
   res.redirect('/api/blocks')
 })
 
-const DEFAULT_PORT=3000
+const syncChains = () => {
+  request({ url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
+    if(!error && response.statusCode === 200) {
+      const rootChain = JSON.stringify(body)
+      console.log("replace chain on a sync with", rootChain)
+      blockchain.replaceChain(rootChain)
+    }
+  })
+}
+
 const port = process.env.GENERATE_PEER_PORT === 'true' ? DEFAULT_PORT + Math.ceil(Math.random() * 1000) : DEFAULT_PORT
 
 app.listen(port, () => {
   console.log(`application has started at localhost:${port}`)
+  syncChains()
 })
