09a336de25a56921a2914d08538364ff8a64bea6
Natthapong Intharak <nattha.int@gmail.com>
2019-11-13 00:19:22 +0700

    Mine api

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const Blockchain = require('./blockchain')
+const bodyParser = require('body-parser')
 
 const app = express()
 const blockchain = new Blockchain()
 
+app.use(bodyParser.json())
+
 app.get('/api/blocks', (req, res) => {
   res.json(blockchain.chain)
+})
+
+app.post('/api/mine', (req, res) => {
+  const { data } = req.body
+  blockchain.addBlock({data})
+  res.redirect('/api/blocks')
 })
 
 const port = 3000
