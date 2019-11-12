b3b81d37f5494aeeb481588dd3cc6c7161092eb5
Natthapong Intharak <nattha.int@gmail.com>
2019-11-12 17:41:08 +0700

    Add average work script

---
+++ average-work.js
@@ -0,0 +1,23 @@
+const Blockchain = require('./blockchain')
+
+const blockchain = new Blockchain()
+
+blockchain.addBlock({data: 'fooData'})
+
+let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average;
+
+const times = []
+
+for (let i=0; i<=10000; i++) {
+  prevTimestamp = blockchain.chain[blockchain.chain.length - 1].timestamp
+  blockchain.addBlock({ data: `block ${i}`})
+
+  nextBlock = blockchain.chain[blockchain.chain.length - 1]
+  nextTimestamp = nextBlock.timestamp
+  timeDiff = nextTimestamp = nextTimestamp - prevTimestamp
+  times.push(timeDiff)
+
+  average = times.reduce((total, number) => { return total + number})/times.length
+
+  console.log(`Time to mine block: ${timeDiff}ms. Difficulty ${nextBlock.difficulty} Average Time: ${average}.ms`)
+}
