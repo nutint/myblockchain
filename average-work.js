dff4cdeb587e7b50cd00c5761bf0e32f7d716b0a
Natthapong Intharak <nattha.int@gmail.com>
2019-11-12 23:39:36 +0700

    improve difficulty algorithm

--- average-work.js
+++ average-work.js
@@ -4,6 +4,8 @@ const blockchain = new Blockchain()
 
 blockchain.addBlock({data: 'fooData'})
 
+console.log('first block', blockchain.chain[blockchain.chain.length-1])
+
 let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average;
 
 const times = []
